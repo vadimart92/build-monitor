0ebc9e0cffcf82d9615082b534dcac9fa273fda8
v.artemchuk <v.artemchuk@bpmonline.com>
2020-07-14 21:42:58 +0300

    angular 10

--- BuildMonitor/ClientApp/src/app/uiutils.ts
+++ BuildMonitor/ClientApp/src/app/uiutils.ts
@@ -1,5 +1,7 @@
 import {BaseConfigItem, BuildServer} from "./data-contracts";
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class UIUtils {
   getConfigText(configItem: BaseConfigItem){
     return JSON.stringify(configItem.config, null, 4);
